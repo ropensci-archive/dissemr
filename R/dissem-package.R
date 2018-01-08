8171f48b4d40c06c1bc2c2e9d2d4dacf572e5445
Scott Chamberlain <myrmecocystus@gmail.com>
2018-01-08 10:14:31 -0800

    change pkg name

--- R/dissem-package.R
+++ R/dissem-package.R
@@ -1,7 +1,7 @@
-#' dissem
+#' dissemr
 #'
-#' @name dissem-package
-#' @aliases dissem
+#' @name dissemr-package
+#' @aliases dissemr
 #' @docType package
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @keywords package
--- tests/test-all.R
+++ tests/test-all.R
@@ -1,2 +1,2 @@
 library("testthat")
-test_check("dissem")
+test_check("dissemr")
